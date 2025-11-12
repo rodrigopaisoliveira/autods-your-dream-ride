7fb3a92e4a24c7ced3bedd91d037784b0c4e9365
rodrigopaisoliveira <rpoas@iscte-iul.pt>
2025-11-12 14:58:40 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // o site deve ser servido a partir da raiz "/"
+
   base: "/",
 
   server: {
